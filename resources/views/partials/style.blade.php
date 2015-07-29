381adbc54c3286e15aaa4c0f35976ffa90edffcd
gamingclen <gaming.clen@gmail.com>
2015-07-29 14:13:25 +0800

    First commit

--- resources/views/partials/style.blade.php
+++ resources/views/partials/style.blade.php
@@ -6,3 +6,14 @@
 <link rel="stylesheet" href="{{ URL::asset('css/index-style.css') }}">
 <link rel="stylesheet" href="{{ URL::asset('css/owl.carousel.css') }}">
 <link rel="stylesheet" href="{{ URL::asset('css/slick.css') }}">
+
+
+<!-- 
+<link rel="stylesheet" href="{{ URL::asset('../resources/assets/css/other-bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('../resources/assets/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('../resources/assets/css/style.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('../resources/assets/css/bootstrap-timepicker.min.css') }}">
+
+<link rel="stylesheet" href="{{ URL::asset('../resources/assets/css/index-style.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('../resources/assets/css/owl.carousel.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('../resources/assets/css/slick.css') }}"> -->
